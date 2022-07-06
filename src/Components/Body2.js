37c782cb1e2b23580631350f5e9b7ec3e7361064
manishbhurtel <manishbhurtel668@gmail.com>
2022-07-06 19:12:12 +0545

    ma

---
+++ src/Components/Body2.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Components/CSS/Body2.css'
+
+function Body2() {
+  return (
+    <>
+     <div className="body2_container">
+        <div className="textPara">
+            <p>So Why should I be hired?</p>
+            <p> What are my strengths?</p>
+        </div>
+        <div className="cards">
+            
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Body2
